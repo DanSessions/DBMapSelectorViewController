9d130616e346d4ace1907fc6e2ce9484bc77ae50
Dan Sessions <dansessions@gmail.com>
2016-08-16 00:41:37 -0500

    Started again.

---
+++ DBCircularMapRegion/DBCircularMapRegion.h
@@ -0,0 +1,19 @@
+//
+//  DBCircularMapRegion.h
+//  DBCircularMapRegion
+//
+//  Created by Dan on 8/16/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DBCircularMapRegion.
+FOUNDATION_EXPORT double DBCircularMapRegionVersionNumber;
+
+//! Project version string for DBCircularMapRegion.
+FOUNDATION_EXPORT const unsigned char DBCircularMapRegionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DBCircularMapRegion/PublicHeader.h>
+
+#import "DBMapSelectorManager.h"
