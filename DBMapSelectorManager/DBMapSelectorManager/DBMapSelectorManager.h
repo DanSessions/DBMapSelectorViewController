1d438cf11b41084963b2caba9c94389f598bd6da
Dan Sessions <dansessions@gmail.com>
2016-08-15 22:09:45 -0500

    Added Carthage support.

---
+++ DBMapSelectorManager/DBMapSelectorManager/DBMapSelectorManager.h
@@ -0,0 +1,19 @@
+//
+//  DBMapSelectorManager.h
+//  DBMapSelectorManager
+//
+//  Created by Dan on 8/15/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DBMapSelectorManager.
+FOUNDATION_EXPORT double DBMapSelectorManagerVersionNumber;
+
+//! Project version string for DBMapSelectorManager.
+FOUNDATION_EXPORT const unsigned char DBMapSelectorManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DBMapSelectorManager/PublicHeader.h>
+
+
