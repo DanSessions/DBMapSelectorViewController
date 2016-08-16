36d247594ebe08a3a14935e93bce1afacfa041ac
Dan Sessions <dansessions@gmail.com>
2016-08-16 00:18:20 -0500

    Trying to fix a header import error.

--- DBMapSelectorManager/DBMapSelectorManager.h
+++ DBMapSelectorManager/DBMapSelectorManager.h
@@ -16,8 +16,8 @@ FOUNDATION_EXPORT const unsigned char DBMapSelectorManagerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <DBMapSelectorManager/PublicHeader.h>
 
-#import "DBMapSelectorManager.h"
-#import "DBMapSelectorGestureRecognizer.h"
-#import "DBMapSelectorAnnotation.h"
-#import "DBMapSelectorOverlay.h"
-#import "DBMapSelectorOverlayRenderer.h"
+#import <DBMapSelectorManager/DBMapSelectorManager.h>
+#import <DBMapSelectorManager/DBMapSelectorGestureRecognizer.h>
+#import <DBMapSelectorManager/DBMapSelectorAnnotation.h>
+#import <DBMapSelectorManager/DBMapSelectorOverlay.h>
+#import <DBMapSelectorManager/DBMapSelectorOverlayRenderer.h>
